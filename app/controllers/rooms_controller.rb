b2d4396d863ed600778c21f043a3b61515f14678
seunadex <seun.adekunle@andela.com>
2017-09-14 19:56:53 +0100

    Add create room functionality'

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,36 @@
 class RoomsController < ApplicationController
+  before_action :find_room, only: [:show, :edit, :update, :destroy]
     def index
-      @rooms = Room.all
-    end
-    def room
-      
+      @rooms = Room.all.order("created_at DESC")
     end
 
     def show
+      
+    end
+
+    def new
+      @room = Room.new
+    end
+
+    def create
+      @room = Room.new(room_params)
+        if @room.save
+          redirect_to rooms_path
+        else
+          render 'new'
+        end
+    end
+
+    def edit
+      
+    end
+
+    private
+    def room_params
+      params.require(:room).permit(:name, :location, :capacity)
+    end
+
+    def find_room
       @room = Room.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :welcome
+  resources :rooms
   root 'welcome#index'
   get '/rooms' => 'rooms#index'
-  get '/rooms/:id' => 'rooms#show', as: :room
-  resources :rooms
+  # post '/rooms/new' => 'rooms#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
